5fab671622655e9bb35d41b5e228104152845801
Ahmed Elkenawy <aelkenawy.c@stc.com.sa>
2025-08-27 13:53:42 +0300

    fix icon
    fix

--- src/app/features/landing/components/investment-focus/investment-focus.component.ts
+++ src/app/features/landing/components/investment-focus/investment-focus.component.ts
@@ -5,6 +5,7 @@ import { InvestmentFocusItemCardComponent } from './item-card/item-card.componen
 import { InvestmentFocusPreviewItemCardComponent } from './preview-item-card/preview-item-card.component';
 
 interface FocusArea {
+  id: number;
   icon: string;
   alt: string;
   labelKey: string;
@@ -20,17 +21,17 @@ interface FocusArea {
 })
 export class InvestmentFocusComponent {
   focusAreas: FocusArea[] = [
-    { icon: 'assets/icons/FinTech-icon.svg', alt: 'FinTech', labelKey: 'investmentFocus.areas.fintech', imageUrl: 'assets/images/investments_focus/fintech.png' },
-    { icon: 'assets/icons/Setting.svg', alt: 'IT Services', labelKey: 'investmentFocus.areas.itServices' , imageUrl: 'assets/images/investments_focus/it_services.png'},
-    { icon: 'assets/icons/Games.svg', alt: 'Gaming', labelKey: 'investmentFocus.areas.gaming' , imageUrl: 'assets/images/investments_focus/gaming.png'},
-    { icon: 'assets/icons/Data Analytics.svg', alt: 'Data Analytics', labelKey: 'investmentFocus.areas.dataAnalytics' , imageUrl: 'assets/images/investments_focus/data_analytics.png'},
-    { icon: 'assets/icons/Cloud.svg', alt: 'Cloud', labelKey: 'investmentFocus.areas.cloud' , imageUrl: 'assets/images/investments_focus/cloud.png'},
-    { icon: 'assets/icons/AI.svg', alt: 'AI', labelKey: 'investmentFocus.areas.ai' , imageUrl: 'assets/images/investments_focus/ai.png'},
-    { icon: 'assets/icons/Cyber Security.svg', alt: 'Cyber Security', labelKey: 'investmentFocus.areas.cyberSecurity' , imageUrl: 'assets/images/investments_focus/cyber_security.png'},
-    { icon: 'assets/icons/Media.svg', alt: 'Media', labelKey: 'investmentFocus.areas.media' , imageUrl: 'assets/images/investments_focus/media.png'},
-    { icon: 'assets/icons/Prop Tech.svg', alt: 'Prop Tech', labelKey: 'investmentFocus.areas.propTech' , imageUrl: 'assets/images/investments_focus/prop_tech.png'},
-    { icon: 'assets/icons/IoT.svg', alt: 'IoT', labelKey: 'investmentFocus.areas.iot' , imageUrl: 'assets/images/investments_focus/iot.png'},
-    { icon: 'assets/icons/BlockChain.svg', alt: 'Blockchain', labelKey: 'investmentFocus.areas.blockchain' , imageUrl: 'assets/images/investments_focus/blockchain.png'}
+    {id : 1 , icon: 'assets/icons/FinTech-icon.svg', alt: 'FinTech', labelKey: 'investmentFocus.areas.fintech', imageUrl: 'assets/images/investments_focus/fintech.png' },
+    {id : 2 , icon: 'assets/icons/Setting.svg', alt: 'IT Services', labelKey: 'investmentFocus.areas.itServices' , imageUrl: 'assets/images/investments_focus/it_services.png'},
+    {id : 3 , icon: 'assets/icons/Games.svg', alt: 'Gaming', labelKey: 'investmentFocus.areas.gaming' , imageUrl: 'assets/images/investments_focus/gaming.png'},
+    {id : 4 , icon: 'assets/icons/Data Analytics.svg', alt: 'Data Analytics', labelKey: 'investmentFocus.areas.dataAnalytics' , imageUrl: 'assets/images/investments_focus/data_analytics.png'},
+    {id : 5 , icon: 'assets/icons/Cloud.svg', alt: 'Cloud', labelKey: 'investmentFocus.areas.cloud' , imageUrl: 'assets/images/investments_focus/cloud.png'},
+    {id : 6 , icon: 'assets/icons/AI.svg', alt: 'AI', labelKey: 'investmentFocus.areas.ai' , imageUrl: 'assets/images/investments_focus/ai.png'},
+    {id : 7 , icon: 'assets/icons/Cyber Security.svg', alt: 'Cyber Security', labelKey: 'investmentFocus.areas.cyberSecurity' , imageUrl: 'assets/images/investments_focus/cyber_security.png'},
+    {id : 8 , icon: 'assets/icons/Media.svg', alt: 'Media', labelKey: 'investmentFocus.areas.media' , imageUrl: 'assets/images/investments_focus/media.png'},
+    {id : 9 , icon: 'assets/icons/Prop Tech.svg', alt: 'Prop Tech', labelKey: 'investmentFocus.areas.propTech' , imageUrl: 'assets/images/investments_focus/prop_tech.png'},
+    {id : 10 , icon: 'assets/icons/IoT.svg', alt: 'IoT', labelKey: 'investmentFocus.areas.iot' , imageUrl: 'assets/images/investments_focus/iot.png'},
+    {id : 11 , icon: 'assets/icons/BlockChain.svg', alt: 'Blockchain', labelKey: 'investmentFocus.areas.blockchain' , imageUrl: 'assets/images/investments_focus/blockchain.png'}
   ];
   selectedArea: FocusArea | null = this.focusAreas.length > 0 ? this.focusAreas[0] : null;
 
