041e1d8b19ff7f10b75c8a0b4f930f3e75b7caf8
Ahmed Hussein <eng.ahmedhussein.cs@gmail.com>
2025-08-28 11:46:23 +0300

    changes in news

--- src/app/layout/components/footer/footer.component.ts
+++ src/app/layout/components/footer/footer.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { TranslateModule } from '@ngx-translate/core';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { TranslateModule } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-footer',
+  selector: "app-footer",
   standalone: true,
-  imports: [
-    CommonModule,
-    TranslateModule
-  ],
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  imports: [CommonModule, TranslateModule],
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.scss"],
 })
 export class FooterComponent {
   currentYear: number = new Date().getFullYear();
+
+  scrollToSection(sectionId: string, event?: Event): void {
+    if (event) {
+      event.preventDefault();
+    }
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  }
 }
