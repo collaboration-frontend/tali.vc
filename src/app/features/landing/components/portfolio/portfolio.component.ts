18378b902969c62a33f9f909c99b514492fe6749
Ahmed Hussein <eng.ahmedhussein.cs@gmail.com>
2025-09-02 22:55:57 +0300

    edit translate

--- src/app/features/landing/components/portfolio/portfolio.component.ts
+++ src/app/features/landing/components/portfolio/portfolio.component.ts
@@ -1,80 +1,80 @@
-import { Component } from '@angular/core';
-import { NgFor } from '@angular/common';
-import { PortfolioItemCardComponent } from './item-card/item-card.component';
-import { TranslateModule } from '@ngx-translate/core';
+import { Component } from "@angular/core";
+import { NgFor } from "@angular/common";
+import { PortfolioItemCardComponent } from "./item-card/item-card.component";
+import { TranslateModule } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-portfolio',
+  selector: "app-portfolio",
   standalone: true,
   imports: [PortfolioItemCardComponent, NgFor, TranslateModule],
-  templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  templateUrl: "./portfolio.component.html",
+  styleUrls: ["./portfolio.component.scss"],
 })
 export class PortfolioComponent {
   portfolioItems: any[] = [
     {
-      title: 'Tarmeez',
-      description: 'portfolio.items.nile.description',
-      sector: 'investmentFocus.areas.fintech',
+      title: "Tarmeez",
+      description: "portfolio.items.tarmeez.description",
+      sector: "investmentFocus.areas.fintech",
       investmentYear: 2022,
-      imageUrl: 'assets/icons/tarmeez.svg',
-      website: 'https://tarmeez.co'
+      imageUrl: "assets/icons/tarmeez.svg",
+      website: "https://tarmeez.co",
     },
     {
-      title: 'Graphiant',
-      description: 'portfolio.items.nile.description',
-      sector: 'investmentFocus.areas.network',
+      title: "Graphiant",
+      description: "portfolio.items.graphiant.description",
+      sector: "investmentFocus.areas.network",
       investmentYear: 2022,
-      imageUrl: 'assets/images/Graphiant.png',
-      website: 'https://www.graphiant.com'
+      imageUrl: "assets/images/Graphiant.png",
+      website: "https://www.graphiant.com",
     },
     {
-      title: 'Nile',
-      description: 'portfolio.items.nile.description',
-      sector: 'investmentFocus.areas.itServices',
+      title: "Nile",
+      description: "portfolio.items.nile.description",
+      sector: "investmentFocus.areas.itServices",
       investmentYear: 2022,
-      imageUrl: 'assets/images/portfolio-image.png',
-      website: 'https://nilesecure.com'
+      imageUrl: "assets/images/portfolio-image.png",
+      website: "https://nilesecure.com",
     },
     {
-      title: 'Rewaa',
-      description: 'portfolio.items.rewaa.description',
-      sector: 'investmentFocus.areas.cloud',
+      title: "Rewaa",
+      description: "portfolio.items.rewaa.description",
+      sector: "investmentFocus.areas.cloud",
       investmentYear: 2021,
-      imageUrl: 'assets/icons/rewaa.svg',
-      website: 'https://www.rewaatech.com'
+      imageUrl: "assets/icons/rewaa.svg",
+      website: "https://www.rewaatech.com",
     },
     {
-      title: 'NearPay',
-      description: 'portfolio.items.nearpay.description',
-      sector: 'investmentFocus.areas.fintech',
+      title: "NearPay",
+      description: "portfolio.items.nearpay.description",
+      sector: "investmentFocus.areas.fintech",
       investmentYear: 2023,
-      imageUrl: 'assets/icons/nearpay.svg',
-      website: 'https://www.nearpay.io'
+      imageUrl: "assets/icons/nearpay.svg",
+      website: "https://www.nearpay.io",
     },
     {
-      title: 'Celona',
-      description: 'portfolio.items.celona.description',
-      sector: 'investmentFocus.areas.itServices',
+      title: "Celona",
+      description: "portfolio.items.celona.description",
+      sector: "investmentFocus.areas.itServices",
       investmentYear: 2022,
-      imageUrl: 'assets/icons/celona.svg',
-      website: 'https://www.celona.io'
+      imageUrl: "assets/icons/celona.svg",
+      website: "https://www.celona.io",
     },
     {
-      title: 'Northladder',
-      description: 'portfolio.items.northladder.description',
-      sector: 'investmentFocus.areas.itServices',
+      title: "Northladder",
+      description: "portfolio.items.northladder.description",
+      sector: "investmentFocus.areas.itServices",
       investmentYear: 2023,
-      imageUrl: 'assets/icons/nothladder.svg',
-      website: 'https://www.northladder.com'
+      imageUrl: "assets/icons/nothladder.svg",
+      website: "https://www.northladder.com",
     },
     {
-      title: 'Cohere',
-      description: 'portfolio.items.cohere.description',
-      sector: 'investmentFocus.areas.ai',
+      title: "Cohere",
+      description: "portfolio.items.cohere.description",
+      sector: "investmentFocus.areas.ai",
       investmentYear: 2022,
-      imageUrl: 'assets/icons/cohere.svg',
-      website: 'https://cohere.com'
-    }
+      imageUrl: "assets/icons/cohere.svg",
+      website: "https://cohere.com",
+    },
   ];
 }
