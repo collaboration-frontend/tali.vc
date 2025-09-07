11774952f85cfd4f0de239aa38ae9f3a4b3a884d
Ahmed Hussein <eng.ahmedhussein.cs@gmail.com>
2025-09-07 10:42:06 +0300

    remove hash

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,18 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter, withHashLocation, withInMemoryScrolling } from '@angular/router';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { TranslateLoader, TranslateModule, TranslationObject } from '@ngx-translate/core';
-import { Observable } from 'rxjs';
+import { ApplicationConfig, importProvidersFrom } from "@angular/core";
+import {
+  provideRouter,
+  withHashLocation,
+  withInMemoryScrolling,
+} from "@angular/router";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import {
+  TranslateLoader,
+  TranslateModule,
+  TranslationObject,
+} from "@ngx-translate/core";
+import { Observable } from "rxjs";
 
-import { routes } from './app.routes';
+import { routes } from "./app.routes";
 
 class AssetTranslateLoader implements TranslateLoader {
   constructor(private httpClient: HttpClient) {}
@@ -23,22 +31,26 @@ export const appConfig: ApplicationConfig = {
     provideRouter(
       routes,
       withInMemoryScrolling({
-        scrollPositionRestoration: 'enabled',
-        anchorScrolling: 'enabled'
-      }) ,
+        scrollPositionRestoration: "enabled",
+        anchorScrolling: "enabled",
+      }),
       //add hashing to the routes
+      /* `withHashLocation()` is a function that configures the router to use hash-based routing in
+      Angular applications. Hash-based routing involves adding a hash symbol (#) to the URL,
+      followed by the route path. This approach is commonly used to support older browsers or server
+      configurations that do not handle HTML5 pushState properly. */
       withHashLocation()
     ),
     importProvidersFrom(
       HttpClientModule,
       TranslateModule.forRoot({
-        defaultLanguage: 'en',
+        defaultLanguage: "en",
         loader: {
           provide: TranslateLoader,
           useFactory: HttpLoaderFactory,
-          deps: [HttpClient]
-        }
+          deps: [HttpClient],
+        },
       })
-    )
-  ]
+    ),
+  ],
 };
